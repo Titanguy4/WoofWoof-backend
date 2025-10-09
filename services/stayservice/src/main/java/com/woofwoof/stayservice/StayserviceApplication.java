cc89aa4058668ca2f62b3f52b55b187f57f7cf6e
KyZiiKs <86744162+Titanguy4@users.noreply.github.com>
2025-10-09 21:43:33 +0200

    feat: init booking and stay services

---
+++ services/stayservice/src/main/java/com/woofwoof/stayservice/StayserviceApplication.java
@@ -0,0 +1,13 @@
+package com.woofwoof.stayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayserviceApplication.class, args);
+	}
+
+}
---
+++ services/stayservice/src/test/java/com/woofwoof/stayservice/StayserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woofwoof.stayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
