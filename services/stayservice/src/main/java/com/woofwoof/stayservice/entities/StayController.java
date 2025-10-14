cd8d1a31697ff44dca0eb096739ea075da0eac1c
lisandre-begon <lisandre.begon1@gmail.com>
2025-10-14 12:32:56 +0200

    feat: create StayController CRUD operations for Stay entity

--- services/stayservice/src/main/java/com/woofwoof/stayservice/entities/StayController.java
+++ services/stayservice/src/main/java/com/woofwoof/stayservice/entities/StayController.java
@@ -1,15 +1,53 @@
 package com.woofwoof.stayservice.entities;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.woofwoof.stayservice.services.StayService;
+import com.woofwoof.stayservice.models.Stay;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 @RestController
+@RequestMapping("/api/stayservice")
 public class StayController {
     private final StayService stayService;
+
     public StayController(StayService stayService) {
         this.stayService = stayService;
     }
 
+    @GetMapping("/{id}")
+    public Optional<Stay> getStayById(@PathVariable Long id) {
+        return stayService.getStayById(id);
+    }
+
+    @GetMapping
+    public List<Stay> getAllStays() {
+        return stayService.getAllStays();
+    }
+
+    @PostMapping
+    public Stay createStay(@RequestBody Stay stay) {
+        return stayService.createStay(stay);
+    }
     
+    @PutMapping("/{id}")
+    public Stay updateStay(@PathVariable Long id, @RequestBody Stay updatedStay) {
+        return stayService.updateStay(id, updatedStay);
+    }
+    
+    @DeleteMapping("/{id}")
+    public void deleteStay(@PathVariable Long id) {
+        stayService.deleteStay(id);
+    }
 }
