e48c4e1e01b5cb397308fcdcba446f52428f2f09
KyZiiKs <86744162+Titanguy4@users.noreply.github.com>
2025-10-16 22:51:23 +0200

    feat: add docker compose for booking service

--- bookingservice/src/main/java/com/woofwoof/bookingservice/BookingServiceApplication.java
+++ bookingservice/src/main/java/com/woofwoof/bookingservice/BookingServiceApplication.java
@@ -2,12 +2,12 @@ package com.woofwoof.bookingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class BookingserviceApplication {
-
+@RestController
+public class BookingServiceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(BookingserviceApplication.class, args);
+		SpringApplication.run(BookingServiceApplication.class, args);
 	}
-
 }
