7d10c71aee691ce6c57aa88a37d82ef1393a8122
KyZiiKs <86744162+Titanguy4@users.noreply.github.com>
2025-11-24 12:04:11 +0100

    feat: security in gateway

--- apigateway/src/main/java/com/woofwoof/apigateway/config/KeycloakRolesConverter.java
+++ apigateway/src/main/java/com/woofwoof/apigateway/config/KeycloakRolesConverter.java
@@ -10,30 +10,34 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
+/**
+ * Permit to override the spring converteur
+ * To map the keycloak roles from the jwt to the GrantedAuthority of Spring
+ * Security
+ */
 public class KeycloakRolesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
     private final String CLAIM_REALM_ACCESS = "realm_access";
+    private final String CLAIM_ROLES = "roles";
     private final String PREFIX_ROLE = "ROLE_";
 
     @Override
     public Collection<GrantedAuthority> convert(Jwt jwt) {
-
         Collection<GrantedAuthority> grantedAuthorities = new ArrayList<>();
-        Map<String, Object> realmAccess = jwt.getClaims();
 
-        if (realmAccess != null && realmAccess.containsKey(CLAIM_REALM_ACCESS)) {
+        Map<String, Object> realmAccess = jwt.getClaim(CLAIM_REALM_ACCESS);
 
-            Collection<String> roles = (Collection<String>) realmAccess.get(CLAIM_REALM_ACCESS);
+        if (realmAccess != null && realmAccess.containsKey(CLAIM_ROLES)) {
+            Object rolesObj = realmAccess.get(CLAIM_ROLES);
 
-            if (roles != null && !roles.isEmpty() && !(roles instanceof Collection)) {
-                Collection<GrantedAuthority> realmRoles = roles.stream()
-                        .map(role -> new SimpleGrantedAuthority(PREFIX_ROLE + role))
+            if (rolesObj instanceof Collection<?> roles) {
+                grantedAuthorities = roles.stream()
+                        .filter(String.class::isInstance)
+                        .map(role -> new SimpleGrantedAuthority(PREFIX_ROLE + role.toString().toUpperCase()))
                         .collect(Collectors.toList());
-                grantedAuthorities.addAll(realmRoles);
             }
         }
 
         return grantedAuthorities;
     }
-
 }
