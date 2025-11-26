7bfdd10bcfa5c7a2e81891b8632f4ba7e8620348
Yanis <yanis.baroudi@etu.umontpellier.fr>
2025-11-26 10:04:14 +0100

    mediaservice working

--- mediaservice/src/main/resources/schema.sql
+++ mediaservice/src/main/resources/schema.sql
@@ -1,8 +1,8 @@
-CREATE TABLE media (
+CREATE TABLE IF NOT EXISTS media (
     id SERIAL PRIMARY KEY,
     url VARCHAR(255) NOT NULL,
     post_date DATE,
     media_type VARCHAR(50),  -- correspond à l'Enum MediaType stocké en String
     stay_id BIGINT,           -- nullable, utilisé pour WOOFSHARE_PHOTO et STAY_PHOTO
-    user_id BIGINT,     -- nullable, utilisé pour PROFILE_PHOTO
+    user_id BIGINT     -- nullable, utilisé pour PROFILE_PHOTO
 );
